9b744e725c639200068a0bec1a15d38a73d090dc
a.khachaturov <a.khachaturov@qiwi.com>
2020-06-01 16:35:04 +0300

    4.1. Аргументы [#297843]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -8,12 +8,14 @@ public class Converter {
     }
 
     public static int rubleToDollar(int value) {
-        int rsl = -1; /* формула перевода рублей в доллоры. */
+        int rsl = value / 60; /* формула перевода рублей в доллоры. */
         return rsl;
     }
 
     public static void main(String[] args) {
         int euro = Converter.rubleToEuro(140);
+        int dollar  = Converter.rubleToDollar(120);
         System.out.println("140 rubles are " + euro + " euro.");
+        System.out.println("120 rubles are " + dollar + " dollar.");
     }
 }
