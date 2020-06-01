787ba8f9c46d050be94da8dc4a0bf664ccd43a77
a.khachaturov <a.khachaturov@qiwi.com>
2020-06-01 14:28:38 +0300

    3. Арифметические операции. [#297844]

--- src/ru/job4j/Info.java
+++ src/ru/job4j/Info.java
@@ -3,5 +3,16 @@ package ru.job4j;
 public class Info {
     public static void main(String[] args){
         System.out.println("31.05.2020");
+        byte age = 28;
+       char sex = 'M';
+        short height1 = 184;
+        double height2 = 1.84;
+        long countBrick = 2145394356;
+        byte countBattery = 12;
+        long distanceSpaceX = 1949994356;
+        boolean isWorking = true;
+        int sizeFile  = 1351889354;
+
+
     }
 }
