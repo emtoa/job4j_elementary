c1210112456bcead1413e0f3e6881fb43f98c0af
a.khachaturov <a.khachaturov@qiwi.com>
2020-06-06 01:36:35 +0300

    5.5. Простое число [#297888]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,20 +2,6 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = false;
-
-        for (int i=2;i<=number-1;i++){
-            prime = true;
-            if ((number % i) == 0) {
-                prime = false;
-                break;
-            }
-        }
-
-        if (number == 2){
-            prime = true;
-        }
-
-        return prime;
+         return prime;
     }
 }
