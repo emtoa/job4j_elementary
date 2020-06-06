49fcb078840b19fe420daa189832a1c051f7da46
Alexander Khchaturov <a.khachaturov@qiwi.com>
2020-06-07 01:10:44 +0300

    6.0.2. Заполнение массива. [#297820]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,17 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+
+        String[] names = new String[4];
+
+        names[0] = "Petr Arsentev";
+        names[1] = "Pavel Bure";
+        names[2] = "Lev Yashin";
+        names[3] = "Igor Akinfeev";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
