b2758edbbcf2d70bc8d91fd5f011237c92533d4d
Alexander Khchaturov <a.khachaturov@qiwi.com>
2020-06-06 22:53:17 +0300

    5.8. JavaDoc. Code convention [#297896]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,35 +1,52 @@
 package ru.job4j.converter;
 
+/** Класс для конвертации рублей в валюту: доллары и евро.
+ * @author Alexander Khachaturov
+ * @version 1.0
+ */
 public class Converter {
-
+    /**
+     * Функция конвертирует рубли в евро
+     * @param value - сумма в рублях
+     * @return сумма в евро
+     */
     public static int rubleToEuro(int value) {
-        int rsl = value / 70;
+        int rsl = value / 70; /* формула перевода рублей в евро. */
         return rsl;
     }
 
+    /**
+     * Функция конвертирует рубли в доллары
+     * @param value - сумма в рублях
+     * @return сумма в долларах
+     */
     public static int rubleToDollar(int value) {
         int rsl = value / 60; /* формула перевода рублей в доллоры. */
         return rsl;
     }
 
+    /**
+     * Метод позволяет вызывать рабочий код программы и выводить результат
+     * @param args
+     */
     public static void main(String[] args) {
-        int euro = Converter.rubleToEuro(140);
-        int dollar  = Converter.rubleToDollar(120);
-        System.out.println("140 rubles are " + euro + " euro.");
-        System.out.println("120 rubles are " + dollar + " dollar.");
+        int euro = Converter.rubleToEuro(140);                 /*Проиводим конвертацию 140 рублей в евро*/
+        int dollar  = Converter.rubleToDollar(120);            /*Проиводим конвертацию 120 рублей в доллары*/
+        System.out.println("140 rubles are " + euro + " euro.");     /*Выводим результат конвертации 140 рублей в евро*/
+        System.out.println("120 rubles are " + dollar + " dollar."); /*Выводим результат конвертации 120 рублей в доллары*/
 
-        int in = 140;
-        int expected = 2;
-        int out = rubleToEuro(in);
-        boolean passed = expected == out;
+        int in = 140;       /*Входящие данные*/
+        int expected = 2;   /*Проверочные данные*/
+        int out = rubleToEuro(in);  /*Проиводим конвертацию 140 рублей в евро*/
+        boolean passed = expected == out; /*Проерка на правильность конвертации из рублей в евро, если конвертация правильно сработала то true, если нет то false*/
 
-        System.out.println("140 rubles are 2. Test result : " + passed);
+        System.out.println("140 rubles are 2. Test result : " + passed); /*Выводим результат конвертации 140 рублей в евро true или false*/
 
-         in = 120;
-         expected = 2;
-         out = rubleToDollar(in);
-         passed = expected == out;
+         in = 120;      /*Входящие данные*/
+         expected = 2;  /*Проверочные данные*/
+         out = rubleToDollar(in);   /*Проиводим конвертацию 120 рублей в доллары*/
+         passed = expected == out;  /*Проерка на правильность конвертации из рублей в евро, если конвертация правильно сработала то true, если нет то false*/
 
-        System.out.println("120 rubles are 2. Test result : " + passed);
+        System.out.println("120 rubles are 2. Test result : " + passed); /*Выводим результат конвертации 120 рублей в доллары true или false*/
     }
 }
