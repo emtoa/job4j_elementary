bf3614270afffb43ccf954376c284048c0008e3e
emtoa <emtoa@yandex.ru>
2020-06-12 10:53:17 +0300

    2. Перегрузка конструктора. [#297989]

--- src/ru/job4j/tracker/Item.java
+++ src/ru/job4j/tracker/Item.java
@@ -1,15 +1,18 @@
 package ru.job4j.tracker;
 
 public class Item {
-    String id;
-    String name;
+    private String id;
+    private String name;
 
     public Item(){
     }
 
     public Item(String id){
+        this.id = id;
     }
 
     public Item(String id, String name){
+        this.id = id;
+        this.name = name;
     }
 }
